075af591ae8cb240872d9be0ba552e23c5850e89
erikkowalski <erikkowalski@mac.com>
2015-08-14 08:55:58 -0400

    initial commit

---
+++ front-page.php
@@ -0,0 +1,20 @@
+<?php // get_template_part('templates/page', 'header'); ?>
+
+<section class="top">
+<?php if (!have_posts()) : ?>
+<div class="alert alert-warning">
+	<?php _e('Sorry, no results were found.', 'sage'); ?>
+</div>
+<?php get_search_form(); ?>
+<?php endif; ?>
+
+<?php while (have_posts()) : the_post(); ?>
+<?php get_template_part('templates/content', get_post_type() != 'post' ? get_post_type() : get_post_format()); ?>
+<?php endwhile; ?>
+
+<div class="clearfix">
+<?php the_posts_navigation(); ?>
+	</div>
+</section>
+<!-- Links & other resources -->
+
---
+++ templates/footer.php
@@ -0,0 +1,8 @@
+<footer class="content-info" role="contentinfo">
+  <div class="container">
+    <?php dynamic_sidebar('sidebar-footer'); ?>
+
+  </div>
+
+	<h5>&copy; Professor Erik Kowalski <?php echo date("Y"); ?></h5>
+</footer>
