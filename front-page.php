918a76f5d6df02d2d40e73bfd44472efb668ebfd
erikkowalski <erikkowalski@mac.com>
2015-08-20 16:40:38 -0400

    Removed max width from home page, Added Loop for TWD Custom post type

--- front-page.php
+++ front-page.php
@@ -1,6 +1,30 @@
 <?php // get_template_part('templates/page', 'header'); ?>
 
-<section class="home">
+<section class="readings">
+
+		<?php //Display 3 random Costar News Posts
+		$twd_reading_args = [
+			'post_type' => 'twd_reading',
+			'posts_per_page' => 1
+		];
+
+		$twd_reading = new WP_Query( $twd_reading_args );
+		?>
+		<h3>TWD Reading Assignemnts</h3>
+
+
+			<?php while ( $twd_reading->have_posts() ) : $twd_reading->the_post(); ?>
+				<h4 class="entry-title"> <a href="<?php the_permalink(); ?>"><?php the_title();?> </a></h4>
+				<div class="entry-content">
+					<?php the_excerpt();?>
+				</div>
+			<?php endwhile;
+wp_reset_postdata();
+			?>
+
+</section>
+
+<section class="blog-feed">
 <?php if (!have_posts()) : ?>
 <div class="alert alert-warning">
 	<?php _e('Sorry, no results were found.', 'sage'); ?>
